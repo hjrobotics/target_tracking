72f4cfec5a36de6278ab0080f0d7231cdfdcd84e
hjrobotics <42360244+hjrobotics@users.noreply.github.com>
2018-09-27 14:01:08 -0700

    add tracking with trigger option
    In VR_tracking_auto_cor_cor-trigger we can choose whether to have robot track the controller or not by using trigger button on controller

--- library/transform_coordinate.py
+++ library/transform_coordinate.py
@@ -8,24 +8,26 @@ from quaternion import quaternion
 class transform_coordinate:
     # l0 is the base location (x0, y0, z0) of the new coordinate based on the old one
     # ori0 is the quaternion of the new coordinate in old coordinate
-    def __init__ (self, l0, ori0):
+    def __init__ (self, l0, ori0, rot):
         assert len(l0) == 3
         assert isinstance(ori0, quaternion)
+        assert isinstance(rot, quaternion)
         self.l0 = l0
         self.ori0 = ori0
+        self.rot = rot
     
     def to_new(self, l, ori):
         assert len(l) == 3
         assert isinstance(ori, quaternion)
         
-        ori0_neg = self.ori0.conjugate()
+        rot_neg = self.rot.conjugate()
         
         dl_old_cor = [x-x0 for x, x0 in zip(l, self.l0)]
-        dl_new_cor = ori0_neg.rotate_vector(dl_old_cor)
+        dl_new_cor = rot_neg.rotate_vector(dl_old_cor)
         
         dori_old_cor = ori.delta(self.ori0)
         dori_old_cor_aangle = dori_old_cor.to_axis_angle()
-        dori_new_cor_aangle = ori0_neg.rotate_vector(dori_old_cor_aangle)
+        dori_new_cor_aangle = rot_neg.rotate_vector(dori_old_cor_aangle)
         dori_new_cor = quaternion.axis_angle_to_quaternion(dori_new_cor_aangle)
         
         return dl_new_cor, dori_new_cor
@@ -34,11 +36,11 @@ class transform_coordinate:
         assert len(l) == 3
         assert isinstance(ori, quaternion)
         
-        dl_old_cor = self.ori0.rotate_vector(l)
+        dl_old_cor = self.rot.rotate_vector(l)
         l_old_cor = [x+x0 for x, x0 in zip(dl_old_cor, self.l0)]
         
         ori_aangle = ori.to_axis_angle()
-        dori_old_cor_aangle = self.ori0.rotate_vector(ori_aangle)
+        dori_old_cor_aangle = self.rot.rotate_vector(ori_aangle)
         dori_old_cor = quaternion.axis_angle_to_quaternion(dori_old_cor_aangle)
         ori_old_cor = dori_old_cor.multiply(self.ori0)
         
